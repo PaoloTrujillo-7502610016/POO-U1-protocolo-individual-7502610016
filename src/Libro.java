53ce6c47373f2dc2e34720953b488c0718fb3ff5
Paolo Trujillo Artunduaga <ptrujilloa@unicartagena.edu.co>
2026-09-08 21:02:44 -0500

    Creacion de clase CuentaBancaria

--- src/Libro.java
+++ src/Libro.java
@@ -1,2 +1,22 @@
 public class Libro {
+    String titulo;
+    String autor;
+    int numeroPaginas;
+
+    //Constructor por defecto
+    public Libro() {
+        titulo = "Harry Potter";
+        autor = "JK Rowling";
+        numeroPaginas = 150;
+    }
+
+    //Constructor parametrizado
+    public Libro(String titulo, String autor, int numeroPaginas) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeroPaginas = numeroPaginas;
+    }
+
+
+
 }
