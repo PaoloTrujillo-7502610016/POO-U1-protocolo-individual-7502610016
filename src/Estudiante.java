3afe7dc5e86a7a591cedd2c038d6d60e7abef761
Paolo Trujillo Artunduaga <ptrujilloa@unicartagena.edu.co>
2026-09-13 17:04:12 -0500

    Desarrollo ejercicio 3, clase Estudiante

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -10,22 +10,16 @@ public class Estudiante {
         curso = 1;
     }
 
-    //Constructor parametrizado
+    //Constructor parametrizado con nombre y edad
     public Estudiante(String nombre, int edad){
         this.nombre = nombre;
         this.edad = edad;
     }
 
-
-
     //Constructor parametrizado y uso de this()
-    public Estudiante(int curso){
-        this.curso = curso;
-    }
     public Estudiante(String nombre, int edad, int curso){
-        this.nombre = nombre;
-        this.edad = edad;
-        this(curso);//llama al constructor con el parámetro curso
+        this(nombre, edad);//llama al constructor anterior con los parametros nombre, edad
+        this.curso = curso;
     }
 
 }
