434460aeae01c33296704ffb4140dc2b32426f34
Paolo Trujillo Artunduaga <ptrujilloa@unicartagena.edu.co>
2026-09-13 16:27:22 -0500

    Creacion de clase Estudiante

--- src/CuentaBancaria.java
+++ src/CuentaBancaria.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class CuentaBancaria {
+    String tipoCuenta;
+    int numeroCuenta;
+    double saldo;
+
+    //Constructor por defecto
+    public CuentaBancaria() {
+        tipoCuenta = "Ahorros";
+        numeroCuenta = 123456;
+        saldo = 1750000;
+    }
+
+    //Contructor parametrizado
+    public CuentaBancaria(int numeroCuenta, double saldo) {
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+    }
+
+
 }
---
+++ src/Estudiante.java
@@ -0,0 +1,5 @@
+public class Estudiante {
+    String nombre;
+    int edad;
+    int curso;
+}
